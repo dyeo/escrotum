4bb1c1dc07a827a6e2b8d548189a2c7d3756cdbd
Dan <dan@dyeo.net>
2021-12-18 19:12:46 -0500

    Fix timestamping; add variable substitution

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="escrotum",
-    version="1.0.1",
-    author='Roger Duran',
-    author_email='rogerduran@gmail.com',
-    url='https://github.com/Roger/escrotum',
-    download_url='https://github.com/Roger/escrotum/archive/1.0.1.tar.gz',
+    version="1.0.2",
+    author='Dan Yeomans',
+    author_email='dan@dyeo.net',
+    url='https://github.com/dyeo/escrotum',
+    download_url='https://github.com/dyeo/escrotum/archive/1.0.2.tar.gz',
     keywords=['screenshot', 'screen-recording', 'scrot', 'cli'],
     packages=["escrotum"],
     include_package_data=True,
